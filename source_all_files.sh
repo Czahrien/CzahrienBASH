6e97984dbabffe5dfac0a816043192de35f79af0
Ben <bdm8233@rit.edu>
2013-07-25 12:45:15 -0400

    Fixed issue with scp and other processes that ssh without a tty. New files are not pulled when there is no tty.

--- source_all_files.sh
+++ source_all_files.sh
@@ -1,24 +1,25 @@
-GITDIR=$(readlink -f $(dirname ${BASH_SOURCE}))
+if [[ $(tty) != "not a tty" ]]; then
+    GITDIR=$(readlink -f $(dirname ${BASH_SOURCE}))
+    # pull the latest copy of the files
+    cd $GITDIR
+    git pull origin master
+    cd - > /dev/null
 
-# pull the latest copy of the files
-cd $GITDIR
-git pull origin master
-cd - > /dev/null
+    # source the files
+    . $GITDIR/bashrc
+    . $GITDIR/funcs
+    . $GITDIR/aliases
 
-# source the files
-. $GITDIR/bashrc
-. $GITDIR/funcs
-. $GITDIR/aliases
+    # source a machine-specific file
+    if [[ -e $GITDIR/bashrc_$HOSTNAME ]]; then
+        . $GITDIR/bashrc_$HOSTNAME
+    fi
 
-# source a machine-specific file
-if [[ -e $GITDIR/bashrc_$HOSTNAME ]]; then
-    . $GITDIR/bashrc_$HOSTNAME
+    #make links to resource file.
+    for file in $(ls $GITDIR/resource_* 2> /dev/null); do
+      if [[ ! -e ~/${file#*/resource_} ]]; then
+        echo No ${file#*/resource_} present in $HOME. Creating a link to $file.
+        ln -s $file ~/${file#*/resource_}
+      fi
+    done
 fi
-
-#make links to resource file.
-for file in $(ls $GITDIR/resource_* 2> /dev/null); do
-  if [[ ! -e ~/${file#*/resource_} ]]; then
-    echo No ${file#*/resource_} present in $HOME. Creating a link to $file.
-    ln -s $file ~/${file#*/resource_}
-  fi
-done
